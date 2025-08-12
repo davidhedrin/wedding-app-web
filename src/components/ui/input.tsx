b7532bf061a3368a599df73c7c7275cdac6f2707
Jeiss <davidhedrin123@gmail.com>
2025-08-12 22:14:31 +0700

    Continue input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ export default function Input({ className, type, ...props }: React.ComponentProp
       className={cn(
         "py-2.5 px-3 block w-full border border-gray-300 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600",
         className
-      )}></input>
+      )} {...props} />
     </div>
   )
 }
