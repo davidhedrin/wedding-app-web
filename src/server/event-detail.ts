6d94b1a426dd648e030bbc2eb6da566581299daf
davidhedrin <davidhedrin123@gmail.com>
2026-01-27 16:48:21 +0700

    Continue submit main info wedding

--- src/server/event-detail.ts
+++ src/server/event-detail.ts
@@ -5,6 +5,8 @@ import { db } from "../../prisma/db-init";
 import { DefaultArgs } from "@prisma/client/runtime/client";
 import { auth } from "@/app/api/auth/auth-setup";
 import { DtoMainInfoWedding } from "@/lib/dto";
+import { CloudflareDeleteFile, CloudflareUploadFile } from "./common";
+import Configs from "@/lib/config";
 
 export async function StoreUpdateMainInfoWedding(formData: DtoMainInfoWedding) {
   try{
@@ -13,6 +15,21 @@ export async function StoreUpdateMainInfoWedding(formData: DtoMainInfoWedding) {
     const { user } = session;
 
     const event_id = formData.id ?? 0;
+
+    const findEventData = await db.events.findUnique({
+      where: { id: event_id }
+    });
+    if(findEventData && findEventData.couple_img_name) CloudflareDeleteFile(Configs.s3_bucket, findEventData.couple_img_name).catch(err => {});
+    if(formData.couple_file_img !== null) {
+      if(findEventData && findEventData.couple_img_name) CloudflareDeleteFile(Configs.s3_bucket, findEventData.couple_img_name).catch(err => {});
+
+      var upFile = await CloudflareUploadFile(formData.couple_file_img, "webp", Configs.s3_bucket, "wedding-couple");
+      if(upFile != null && upFile.status == true) {
+        formData.couple_img_name = upFile.filename;
+        formData.couple_img_url = upFile.path;
+      };
+    };
+
     await db.$transaction(async (tr) => {
       await tr.events.update({
         where: { id: event_id },
@@ -20,6 +37,8 @@ export async function StoreUpdateMainInfoWedding(formData: DtoMainInfoWedding) {
           greeting_msg: formData.greeting_msg,
           contact_email: formData.contact_email,
           contact_phone: formData.contact_phone,
+          couple_img_name: formData.couple_img_name,
+          couple_img_path: formData.couple_img_url,
           music_url: formData.music_url,
           updatedBy: user?.email
         }
