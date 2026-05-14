975c9ac1056df9dc744100fcd77b23e49a699c3b
Jeiss <davidhedrin123@gmail.com>
2026-05-14 09:29:46 +0700

    Fix proxy logic

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,21 +1,45 @@
-import { NextResponse, NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import { auth } from "./app/api/auth/auth-setup";
 
-export async function proxy(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
-  const pathname = request.nextUrl.pathname;
+// import { getToken } from "next-auth/jwt";
+
+// export async function proxy(request: NextRequest) {
+//   const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
+//   const pathname = request.nextUrl.pathname;
   
-  if (!token && !pathname.startsWith("/auth")) return redirectTo(request, "/auth");
-  if (token && pathname.startsWith("/auth")) return redirectTo(request, "/");
+//   if (!token && !pathname.startsWith("/auth")) return redirectTo(request, "/auth");
+//   if (token && pathname.startsWith("/auth")) return redirectTo(request, "/");
   
-  if (pathname.startsWith("/client/systems") && token?.role != "Admin") return redirectTo(request, "/");
+//   if (pathname.startsWith("/client/systems") && token?.role != "Admin") return redirectTo(request, "/");
+
+//   return NextResponse.next();
+// }
+
+// function redirectTo(request: NextRequest, url: string) {
+//   const loginUrl = new URL(url, request.nextUrl.origin);
+//   return NextResponse.redirect(loginUrl);
+// }
+
+export default auth((req) => {
+  console.log("AUTH", req.auth);
+
+  const pathname = req.nextUrl.pathname;
+  const isLoggedIn = !!req.auth;
+
+  // Belum login
+  if (!isLoggedIn && !pathname.startsWith("/auth")) return redirectTo(req, "/auth");
+
+  // Sudah login tapi akses auth page
+  if (isLoggedIn && pathname.startsWith("/auth")) return redirectTo(req, "/");
+
+  // Role protection
+  if (pathname.startsWith("/client/systems") && req.auth?.user?.role !== "Admin") return redirectTo(req, "/");
 
   return NextResponse.next();
-}
+});
 
-function redirectTo(request: NextRequest, url: string) {
-  const loginUrl = new URL(url, request.nextUrl.origin);
-  return NextResponse.redirect(loginUrl);
+function redirectTo(req: Request, url: string) {
+  return NextResponse.redirect(new URL(url, req.url));
 }
 
 export const config = {
